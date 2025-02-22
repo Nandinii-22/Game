445d6677a1e2973db806ae0784e9c470bedd336f
Nandini <nandinigupta31858@gmail.com>
2025-02-22 10:26:51 +0530

    Created snake head and added functions for its movement

--- g_code.py
+++ g_code.py
@@ -1,4 +1,7 @@
 import turtle
+import time
+
+delay = 0.1
 
 # Screen Setup
 
@@ -8,4 +11,60 @@ wn.bgcolor("yellow")
 wn.setup(width=600, height=600)
 wn.tracer(0) # turns off the screen animation
 
+ # Snake Head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Functions
+def go_up():
+    if head.direction != "down":
+       head.direction = "up"
+
+def go_down():
+     if head.direction != "up":
+       head.direction = "down"
+    
+def go_right():
+     if head.direction != "left":
+        head.direction = "right"
+    
+def go_left():
+     if head.direction != "right":
+       head.direction = "left"    
+
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y+20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y-20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x+20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x-20)
+
+# Keyboard bindings
+
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_right, "d")
+wn.onkeypress(go_left, "a")
+
+
+# Main Game loop
+while True:
+    wn.update()
+    move()
+    time.sleep(delay)
+    
 wn.mainloop()
