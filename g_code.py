764236ccfe7d39a75a974ee990c6b4d60aa8ebf0
Nandini <nandinigupta31858@gmail.com>
2025-02-22 10:06:49 +0530

    done the screen setup

---
+++ g_code.py
@@ -0,0 +1,11 @@
+import turtle
+
+# Screen Setup
+
+wn  = turtle.Screen()
+wn.title("Snake Game by Nandini_Gupta")
+wn.bgcolor("yellow")
+wn.setup(width=600, height=600)
+wn.tracer(0) # turns off the screen animation
+
+wn.mainloop()
